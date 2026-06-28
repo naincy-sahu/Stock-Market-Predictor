5c51ddd66533604e13301edf025ca9a459616f47
naincy-sahu <snaincy819@gmail.com>
2026-06-28 22:40:31 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,16 +1,26 @@
-
-
- import joblib
+import joblib
 import pandas as pd
 
+# Trained model load karo
 model = joblib.load("models/saved_model.pkl")
 
-ma_10 = float(input("Enter 10 Day Moving Average: "))
+# User Input
+open_price = float(input("Enter Open Price: "))
+high_price = float(input("Enter High Price: "))
+low_price = float(input("Enter Low Price: "))
+volume = float(input("Enter Volume: "))
+ma10 = float(input("Enter 10-Day Moving Average: "))
 
+# DataFrame banao
 input_data = pd.DataFrame({
-    "MA_10": [ma_10]
+    "Open": [open_price],
+    "High": [high_price],
+    "Low": [low_price],
+    "Volume": [volume],
+    "MA_10": [ma10]
 })
 
+# Prediction
 prediction = model.predict(input_data)
 
 print("Predicted Closing Price:", prediction[0])
