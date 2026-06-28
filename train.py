ec3a44efe5dfe0c74d8e3363e731b9d8b9ab9e39
naincy-sahu <snaincy819@gmail.com>
2026-06-28 22:29:25 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,24 +1,20 @@
-joblib.dump(model, "models/saved_model.pkl")
-
-print("Model Saved Successfully")import joblibfrom utils.data_loader import load_data
+from utils.data_loader import load_data
 from utils.preprocessing import preprocess_data
 from utils.indicators import add_moving_average
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+import joblib
 
 stock = "AAPL"
 
 data = load_data(stock)
-
 data = preprocess_data(data)
-
 data = add_moving_average(data)
-
 data = data.dropna()
 
-X = data[["MA_10"]]
-
+X = data[["Open","High","Low","Volume","MA_10"]]
 y = data["Close"]
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -26,20 +22,14 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 model = LinearRegression()
-
 model.fit(X_train, y_train)
 
-print("Model Training Completed")
-from utils.data_loader import load_data
-from utils.preprocessing import preprocess_data
-from utils.indicators import add_moving_average
+predictions = model.predict(X_test)
 
-stock = "AAPL"
+mae = mean_absolute_error(y_test, predictions)
 
-data = load_data(stock)
+print("Mean Absolute Error:", mae)
 
-data = preprocess_data(data)
+joblib.dump(model, "models/saved_model.pkl")
 
-data = add_moving_average(data)
-
-print(data.head())
+print("Model Saved Successfully")
