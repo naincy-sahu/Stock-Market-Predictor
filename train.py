6d66fa36d6d1f845ccce2d4b115e72c19b0277a8
naincy-sahu <snaincy819@gmail.com>
2026-06-28 22:14:01 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from utils.data_loader import load_data
+import joblibfrom utils.data_loader import load_data
 from utils.preprocessing import preprocess_data
 from utils.indicators import add_moving_average
 
