67a07eef6f6de2d33a94e5087eda5c0c3336bc0d
naincy-sahu <snaincy819@gmail.com>
2026-06-28 22:15:36 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,1 +1,7 @@
-print("Predicting Stock Price...")
+import joblib
+
+model = joblib.load("models/saved_model.pkl")
+
+prediction = model.predict([[150]])
+
+print(prediction)print("Predicting Stock Price...")
