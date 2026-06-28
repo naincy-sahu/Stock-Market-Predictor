e3d661ae7e781ebcd083ebe3b72359e890a72275
naincy-sahu <snaincy819@gmail.com>
2026-06-28 22:16:41 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,22 @@
 import joblib
+import pandas as pd
+
+# Saved model load karo
+model = joblib.load("models/saved_model.pkl")
+
+# User input
+ma_10 = float(input("Enter 10 Day Moving Average: "))
+
+# DataFrame banao
+input_data = pd.DataFrame({
+    "MA_10": [ma_10]
+})
+
+# Prediction
+prediction = model.predict(input_data)
+
+# Result
+print("Predicted Closing Price:", prediction[0])import joblib
 
 model = joblib.load("models/saved_model.pkl")
 
