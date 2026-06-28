9660b8bf343c80cd6dd2ae0d361b5ec9c4fe5520
naincy-sahu <snaincy819@gmail.com>
2026-06-28 22:08:40 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,14 @@
 print("Training AI Model...")
+from utils.data_loader import load_data
+from utils.preprocessing import preprocess_data
+from utils.indicators import add_moving_average
+
+stock = "AAPL"
+
+data = load_data(stock)
+
+data = preprocess_data(data)
+
+data = add_moving_average(data)
+
+print(data.head())
