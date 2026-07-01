48f6e41d466e3acf84c27ebad2ef1bf85ebc64c9
naincy-sahu <snaincy819@gmail.com>
2026-07-01 19:50:50 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,4 +1,20 @@
 import yfinance as yf
+from utils.logger import logger
+
+def load_data(stock):
+    try:
+        logger.info(f"Downloading data for {stock}")
+
+        data = yf.download(stock)
+
+        logger.info("Data Download Successful")
+
+        return data
+
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
+        return Noneimport yfinance as yf
 
 def load_data(stock):
     data = yf.download(stock)
